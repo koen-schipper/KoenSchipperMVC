80be71106f1f2eeb4ddcc7b192b208a02310aae2
Koen Schipper <koen.schipper@me.com>
2023-03-12 10:35:22 +0100

    initial commit

---
+++ app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title'] ?></h1>
+<p>This is the KoenSchipperMVC PHP Framework. Please refer to the docs on how to use it.</p>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
